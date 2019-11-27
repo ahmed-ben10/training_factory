ff30f6939a19e1c06b48b6f24fb2bc0c4fc686ae
Ahmed <55>
2019-11-27 15:11:40 +0100

    nav gefixt

--- src/Controller/BezoekerController.php
+++ src/Controller/BezoekerController.php
@@ -12,14 +12,34 @@ class BezoekerController extends AbstractController
     /**
      * @Route("/",name="app_bezoeker_home")
      */
-    public function homepage(){
-        return $this->render('bezoeker/home.html.twig',['page_name'=>'app_bezoeker_home']);
+    public function homepage()
+    {
+        return $this->render('bezoeker/home.html.twig', ['page_name' => 'app_bezoeker_home']);
     }
 
     /**
      * @Route("/registreren",name="app_bezoeker_registreren")
      */
-    public function registreren(){
-        return $this->render('bezoeker/bezoeker_registreren.html.twig',['page_name'=>'app_bezoeker_registreren']);
+    public function registreren()
+    {
+        return $this->render('bezoeker/bezoeker_registreren.html.twig', ['page_name' => 'app_bezoeker_registreren']);
+    }
+
+    /**
+     * @Route("/app_bezoeker_trainings_aanbod",name="app_bezoeker_trainings_aanbod")
+     */
+    public function trainingsAanbod()
+    {
+        return $this->render('bezoeker/home.html.twig', ['page_name' => 'app_bezoeker_trainings_aanbod']);
+
+    }
+
+    /**
+     * @Route("/app_bezoeker_contact",name="app_bezoeker_contact")
+     */
+    public function contact()
+    {
+        return $this->render('bezoeker/home.html.twig', ['page_name' => 'app_bezoeker_contact']);
+
     }
 }
