708f724505906d19ca4a69634844ee09874ba6fd
Ahmed ben <302267915@student.rocmondriaan.nl>
2019-11-27 21:45:47 +0100

    een aantal pagina's gemaakt

--- src/Controller/BezoekerController.php
+++ src/Controller/BezoekerController.php
@@ -10,36 +10,43 @@ use Symfony\Component\Routing\Annotation\Route;
 class BezoekerController extends AbstractController
 {
     /**
-     * @Route("/",name="app_bezoeker_home")
+     * @Route("/",name="bezoeker_home")
      */
     public function homepage()
     {
-        return $this->render('bezoeker/home.html.twig', ['page_name' => 'app_bezoeker_home']);
+        return $this->render('bezoeker/home.html.twig', ['page_name' => 'bezoeker_home']);
     }
 
     /**
-     * @Route("/registreren",name="app_bezoeker_registreren")
-     */
-    public function registreren()
-    {
-        return $this->render('bezoeker/bezoeker_registreren.html.twig', ['page_name' => 'app_bezoeker_registreren']);
-    }
-
-    /**
-     * @Route("/app_bezoeker_trainings_aanbod",name="app_bezoeker_trainings_aanbod")
+     * @Route("/bezoeker_trainings_aanbod",name="bezoeker_trainings_aanbod")
      */
     public function trainingsAanbod()
     {
-        return $this->render('bezoeker/home.html.twig', ['page_name' => 'app_bezoeker_trainings_aanbod']);
+        return $this->render('bezoeker/trainings_aanbod.html.twig', ['page_name' => 'bezoeker_trainings_aanbod']);
 
     }
 
     /**
-     * @Route("/app_bezoeker_contact",name="app_bezoeker_contact")
+     * @Route("/registreren",name="bezoeker_registreren")
+     */
+    public function registreren()
+    {
+        return $this->render('bezoeker/bezoeker_registreren.html.twig', ['page_name' => 'bezoeker_registreren']);
+    }
+
+    /**
+     * @Route("/gedrags_regels",name="bezoeker_regels")
+     */
+    public function gedragsRegels(){
+        return $this->render('bezoeker/bezoeker_regels.html.twig', ['page_name' => 'bezoeker_regels']);
+    }
+
+    /**
+     * @Route("/bezoeker_contact",name="bezoeker_contact")
      */
     public function contact()
     {
-        return $this->render('bezoeker/home.html.twig', ['page_name' => 'app_bezoeker_contact']);
+        return $this->render('bezoeker/contact.html.twig', ['page_name' => 'bezoeker_contact']);
 
     }
 }
